be1023642d1777309edb85df53ccd0f34a58ae8c
matheustomczak-dev <matheus_tomczak@estudante.sesisenai.org.br>
2026-05-23 17:39:17 -0300

    add- tarefas

--- paginai.js
+++ paginai.js
@@ -1,8 +1,49 @@
-const form  = document.querySelector('.task-form')
-const input = document.querySelector('input')
+document.addEventListener("DOMContentLoaded", () => {
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault()
+    const form = document.querySelector(".task-form");
+    const input = form.querySelector("input");
+    const listaTarefas = document.querySelector(".tarefas");
 
-    console.log(input.value)
-})
+    form.addEventListener("submit", (e) => {
+
+        e.preventDefault();
+
+        const texto = input.value.trim();
+
+        // impede tarefa vazia
+        if (texto === "") {
+            return;
+        }
+
+        // cria div da tarefa
+        const novaTarefa = document.createElement("div");
+        novaTarefa.classList.add("tarefa");
+
+        // cria texto
+        const p = document.createElement("p");
+        p.textContent = texto;
+
+        // cria botão
+        const botao = document.createElement("button");
+        botao.textContent = "Excluir";
+
+        botao.classList.add("btn");
+        botao.classList.add("btn-danger");
+
+        // botão excluir
+        botao.addEventListener("click", () => {
+            novaTarefa.remove();
+        });
+
+        // adiciona elementos
+        novaTarefa.appendChild(p);
+        novaTarefa.appendChild(botao);
+
+        listaTarefas.appendChild(novaTarefa);
+
+        // limpa input
+        input.value = "";
+
+    });
+
+});
